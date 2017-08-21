477fe837f917c5ffc5c88f518002917b9b5efed1
Todd <myoung44@vols.utk.edu>
2017-08-21 17:52:19 -0400

    Adding agent

---
+++ tanuki/agent.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+class Agent(object):
+    """
+    The agent. Able to take on of a set of actions at each time step. The action
+    is chosen using a strategy based on a history of prior actions and outcome
+    observations.
+    """
+    def __init__(self, bandit, policy, prior=0, gamma=None):
+        self.policy = policy
+        self.k = bandit.k
+        self.prior = prior
+        self.gamma = gamma
+        self._value_estimates = prior * np.ones(self.k)
+        self.action_attempts = np.zeros(self.k)
+        self.t = 0
+        self.last_action = None
+
+    def __str__(self):
+        return 'f/{}'.format(str(self.policy))
+
+    def reset(self):
+        """
+        Resets the agent's memory to an intial state.
+        """
+        self._value_estimates[:] = self.prior
+        self.action_attempts[:] = 0
+        self.last_action = None
+        self.t = 0
+
+    def choose(self):
+        self.action = self.policy.choose(self)
+        self.last_action = action
+        return action
+
+    def observe(self):
+        self.action_atempts[self.last_action] += 1
+
+        if self.gamma is None:
+            g = 1 / self.action_attempts[self.last_action]
+        else:
+            g = self.gamma
+
+         q = self._value_estimates[self.last_action]
+         self._value_estimates[self.last_action] += g * (reward - q)
+         self.t += 1
+
+    @property
+    def value_estimates(self):
+        return self._value_estimates
