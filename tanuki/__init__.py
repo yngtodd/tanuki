508569c97830ec1f22037e9de1e8e022a371a9ab
Todd <myoung44@vols.utk.edu>
2017-08-22 11:51:40 -0400

    Updating __init__

--- tanuki/__init__.py
+++ tanuki/__init__.py
@@ -1,10 +1,16 @@
+from . import agent
 from . import bandit
+from . import environment
+from . import policy
 
 __version__ = "0.1.0"
 
 
 __all__ = (
-            "bandit"
-            )
+    "agent",
+    "bandit",
+    "environment",
+    "policy"
+)
 
 
